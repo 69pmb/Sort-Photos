93d446551b08f0b7d10e13c5ccda6bbdb0cad0f0
pierre-marie broca <pierre-marie.broca@worldline.com>
2020-10-26 18:07:23 +0100

    feat: handling duplicates

--- src/main/java/pmb/sort/photos/Controller.java
+++ src/main/java/pmb/sort/photos/Controller.java
@@ -4,16 +4,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.TimeZone;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,25 +25,26 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.exif.ExifDirectoryBase;
-import com.drew.metadata.exif.ExifSubIFDDirectory;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import pmb.my.starter.exception.MinorException;
 import pmb.my.starter.utils.MyConstant;
 import pmb.my.starter.utils.MyFileUtils;
 import pmb.my.starter.utils.MyProperties;
 
-public class Controller implements Initializable {
+public class Controller
+        implements Initializable {
+
     private static final Logger LOG = LogManager.getLogger(Controller.class);
     @FXML
     private GridPane container;
@@ -66,25 +69,25 @@ public class Controller implements Initializable {
     @FXML
     private RadioButton radioRoot;
     private File selectedDir;
-    private ResourceBundle resources;
+    private ResourceBundle bundle;
     private Map<TextField, Property> properties;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        this.resources = resources;
-        properties = Map.of(displayDir, Property.DEFAULT_WORKING_DIR, dateFormat, Property.DATE_FORMAT, yearFormat,
-                Property.YEAR_FOLDER_FORMAT, monthFormat, Property.MONTH_FOLDER_FORMAT, pictureExtention,
-                Property.PICTURE_EXTENTION, videoExtention, Property.VIDEO_EXTENTION);
+        this.bundle = resources;
+        properties = Map.of(dateFormat, Property.DATE_FORMAT, yearFormat, Property.YEAR_FOLDER_FORMAT, monthFormat, Property.MONTH_FOLDER_FORMAT,
+                pictureExtention, Property.PICTURE_EXTENTION, videoExtention, Property.VIDEO_EXTENTION);
         properties.forEach((field, prop) -> field.setText(MiscUtils.getDefaultValue(prop)));
-        selectedDir = new File(
-                MyProperties.getOrDefault(Property.DEFAULT_WORKING_DIR.getValue(), MyConstant.USER_DIRECTORY));
+        selectedDir = MyProperties.get(Property.DEFAULT_WORKING_DIR.getValue()).map(File::new).filter(File::exists)
+                .orElse(new File(MyConstant.USER_DIRECTORY));
+        displayDir.setText(selectedDir.getAbsolutePath());
         detectFolder();
     }
 
     @FXML
     private void selectDirectory() {
         FileChooser dirChooser = new FileChooser();
-        dirChooser.setTitle(resources.getString("directory.chooser.title"));
+        dirChooser.setTitle(bundle.getString("directory.chooser.title"));
         dirChooser.setInitialDirectory(selectedDir);
         selectedDir = dirChooser.showOpenDialog(container.getScene().getWindow());
         if (selectedDir != null) {
@@ -107,31 +110,30 @@ public class Controller implements Initializable {
         Supplier<Stream<TextField>> fields = () -> properties.keySet().stream().filter(k -> !displayDir.equals(k));
         List<TextField> blanks = fields.get().filter(MiscUtils.isBlank).collect(Collectors.toList());
         List<TextField> invalidDates = List.of(dateFormat, yearFormat, monthFormat).stream()
-                .filter(MiscUtils.validDateFormat.negate().or(MiscUtils.invalidCharacters))
-                .collect(Collectors.toList());
+                .filter(MiscUtils.validDateFormat.negate().or(MiscUtils.invalidCharacters)).collect(Collectors.toList());
         List<TextField> invalidExtentions = List.of(pictureExtention, videoExtention).stream()
                 .filter(MiscUtils.validExtention.negate().or(MiscUtils.invalidCharacters)).collect(Collectors.toList());
         fields.get().forEach(f -> f.getStyleClass().remove("error"));
         Stream.of(blanks, invalidDates, invalidExtentions).flatMap(List::stream).collect(Collectors.toSet())
-        .forEach(f -> f.getStyleClass().add("error"));
+                .forEach(f -> f.getStyleClass().add("error"));
 
         List<String> messages = new ArrayList<>();
         if (!blanks.isEmpty()) {
-            messages.add(resources.getString("warning.empty"));
+            messages.add(bundle.getString("warning.empty"));
         }
         if (!invalidDates.isEmpty()) {
-            messages.add(resources.getString("warning.date.format"));
+            messages.add(bundle.getString("warning.date.format"));
         }
         if (!invalidExtentions.isEmpty()) {
-            messages.add(resources.getString("warning.extention"));
+            messages.add(bundle.getString("warning.extention"));
         }
 
         if (!messages.isEmpty()) {
-            messageProperties.setText(resources.getString("warning") + StringUtils.join(messages, ","));
+            messageProperties.setText(bundle.getString("warning") + StringUtils.join(messages, ","));
         } else {
-            messageProperties.setText(resources.getString("properties.saved"));
+            messageProperties.setText(bundle.getString("properties.saved"));
             properties.entrySet().stream().filter(e -> e.getValue() != Property.DEFAULT_WORKING_DIR)
-            .forEach(e -> MyProperties.set(e.getValue().getValue(), e.getKey().getText()));
+                    .forEach(e -> MyProperties.set(e.getValue().getValue(), e.getKey().getText()));
             MyProperties.save();
             detectFolder();
         }
@@ -142,35 +144,16 @@ public class Controller implements Initializable {
         SimpleDateFormat sdf = new SimpleDateFormat(dateFormat.getText());
         SimpleDateFormat yearSdf = new SimpleDateFormat(yearFormat.getText());
         SimpleDateFormat monthSdf = new SimpleDateFormat(monthFormat.getText());
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(dateFormat.getText());
-        DateTimeFormatter yearDtf = DateTimeFormatter.ofPattern(yearFormat.getText());
-        DateTimeFormatter monthDtf = DateTimeFormatter.ofPattern(monthFormat.getText());
-        List<String> extentions = Arrays.asList(ArrayUtils.addAll(StringUtils.split(pictureExtention.getText(), ","),
-                StringUtils.split(videoExtention.getText(), ",")));
+        List<String> extentions = Arrays
+                .asList(ArrayUtils.addAll(StringUtils.split(pictureExtention.getText(), ","), StringUtils.split(videoExtention.getText(), ",")));
         MyFileUtils.listFilesInFolder(selectedDir, extentions, false).forEach(file -> {
-            String absolutePath = file.getAbsolutePath();
+            Date date = MiscUtils.getTakenTime(file).orElseGet(() -> MyFileUtils.getCreationDate(file));
+            String newName = sdf.format(date);
+            LOG.debug("File {} taken at {}", file.getName(), newName);
             try {
-                Metadata metadata = ImageMetadataReader.readMetadata(file);
-                Directory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-                String newName;
-                String yearFolder;
-                String monthFolder;
-                if (directory != null) {
-                    Date takenTime = directory.getDate(ExifDirectoryBase.TAG_DATETIME_ORIGINAL, TimeZone.getDefault());
-                    newName = sdf.format(takenTime);
-                    yearFolder = yearSdf.format(takenTime);
-                    monthFolder = monthSdf.format(takenTime);
-                    LOG.info("file {} taken at {}", file.getName(), newName);
-                } else {
-                    LocalDateTime creationDate = MyFileUtils.getCreationDate(file);
-                    newName = creationDate.format(dtf);
-                    yearFolder = creationDate.format(yearDtf);
-                    monthFolder = creationDate.format(monthDtf);
-                    LOG.info("Pas d'exif {}, creation date {}", absolutePath, newName);
-                }
-                renameFile(file, newName, yearFolder, monthFolder);
-            } catch (ImageProcessingException | IOException e) {
-                LOG.error("Error reading metadata of file: {}", absolutePath);
+                renameFile(file, newName, yearSdf.format(date), monthSdf.format(date));
+            } catch (IOException e) {
+                throw new MinorException("Error when renaming file " + file.getAbsolutePath() + " to " + newName, e);
             }
         });
     }
@@ -188,17 +171,82 @@ public class Controller implements Initializable {
             MyFileUtils.createFolderIfNotExists(monthPath);
             newPath = monthPath + MyConstant.FS + newFilename;
         } else if (radioYear.isSelected()) {
-            MyFileUtils.createFolderIfNotExists(selectedDir.getAbsolutePath() + MyConstant.FS + monthFolder);
-            newPath = selectedDir.getAbsolutePath() + MyConstant.FS + monthFolder + MyConstant.FS + newFilename;
+            String monthPath = selectedDir.getAbsolutePath() + MyConstant.FS + monthFolder;
+            MyFileUtils.createFolderIfNotExists(monthPath);
+            newPath = monthPath + MyConstant.FS + newFilename;
         } else {
             newPath = selectedDir.getAbsolutePath() + MyConstant.FS + newFilename;
         }
 
-        LOG.info("Path {}", newPath);
         File newFile = new File(newPath);
-        if (!newFile.exists()) {
-            Files.move(file.toPath(), newFile.toPath());
+        if (!StringUtils.equals(newPath, absolutePath) && !Files.isSameFile(file.toPath(), newFile.toPath())) {
+            LOG.info("Path {}", newPath);
+            if (!newFile.exists()) {
+                Files.move(file.toPath(), newFile.toPath());
+            } else {
+                duplicateDialog(file, absolutePath, extention, newPath, newFile);
+            }
         }
+    }
+
+    private void duplicateDialog(File file, String absolutePath, String extention, String newPath, File newFile) throws IOException {
+        Stage dialog = new Stage();
+        dialog.initOwner(container.getScene().getWindow());
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.add(JavaFxUtils.displayPicture(file, "box", 600), 1, 1);
+        gridPane.add(JavaFxUtils.displayPicture(newFile, "box", 600), 2, 1);
+        gridPane.add(buildDescriptions(file), 1, 2);
+        gridPane.add(buildDescriptions(newFile), 2, 2);
+        HBox hBox = new HBox();
+        hBox.getChildren().add(new Text(bundle.getString("duplicate.warning")));
+        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.cancel"), e -> dialog.close());
+        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.overwrite"), e -> {
+            try {
+                dialog.close();
+                Files.move(file.toPath(), newFile.toPath());
+            } catch (IOException e1) {
+                throw new MinorException("Error when moving file " + absolutePath + " to " + newPath, e1);
+            }
+        });
+        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.rename"), e -> {
+            Integer index = 1;
+            File renamedFile;
+            do {
+                renamedFile = new File(StringUtils.substringBeforeLast(newPath, MyConstant.DOT) + "-" + index + MyConstant.DOT + extention);
+                index++;
+            } while (renamedFile.exists());
+            try {
+                dialog.close();
+                Files.move(file.toPath(), renamedFile.toPath());
+            } catch (IOException e1) {
+                throw new MinorException("Error when moving file " + absolutePath + " to " + newPath, e1);
+            }
+        });
+        hBox.setSpacing(10);
+        gridPane.add(hBox, 1, 3);
+        Scene scene = new Scene(gridPane);
+        scene.getStylesheets().add(Controller.class.getResource("application.css").toExternalForm());
+        dialog.setScene(scene);
+        dialog.showAndWait();
+    }
+
+    public Text buildDescriptions(File file) throws IOException {
+        Text text = new Text();
+        text.setWrappingWidth(400);
+        text.setTextAlignment(TextAlignment.JUSTIFY);
+        BasicFileAttributes basic = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        Function<Instant, String> format = date -> DateFormat.getInstance().format(Date.from(date));
+        List<String> sb = new ArrayList<>();
+        BiConsumer<String, String> append = (key, value) -> sb.add(bundle.getString(key) + ": " + value);
+        append.accept("duplicate.creation_date", format.apply(basic.creationTime().toInstant()));
+        append.accept("duplicate.modification_date", format.apply(basic.lastModifiedTime().toInstant()));
+        append.accept("duplicate.taken_time", MiscUtils.getTakenTime(file).map(Date::toInstant).map(format::apply).orElse("Not Found"));
+        append.accept("duplicate.model", MiscUtils.getModel(file).orElse("Unknown"));
+        append.accept("duplicate.size", (basic.size() / 1024) + " KB");
+        text.setText(sb.stream().collect(Collectors.joining("\n")));
+        return text;
     }
 
     private void detectFolder() {
@@ -210,4 +258,5 @@ public class Controller implements Initializable {
             radioRoot.setSelected(true);
         }
     }
+
 }
