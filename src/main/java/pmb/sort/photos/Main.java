483f78848751ee141a278942c4aa77bdb668c329
pierre-marie broca <pierre-marie.broca@worldline.com>
2020-10-29 16:09:15 +0100

    feat: logging

--- src/main/java/pmb/sort/photos/Main.java
+++ src/main/java/pmb/sort/photos/Main.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main
+        extends Application {
+
+    private static final Logger LOG = LogManager.getLogger(Main.class);
 
     @Override
     public void start(Stage stage) throws IOException {
+        LOG.debug("Start start");
         Locale locale = Locale.getDefault();
         ResourceBundle bundle = ResourceBundle.getBundle("i18n", locale);
         Parent root = FXMLLoader.load(getClass().getResource("Screen.fxml"), bundle);
@@ -21,9 +28,11 @@ public class Main extends Application {
         scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
+        LOG.debug("End start");
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
