9c3579ab376b9158097a2099d6e2ca4cfc707a2e
pierre-marie broca <pierre-marie.broca@worldline.com>
2020-11-02 17:44:37 +0100

    test: Picture

--- src/test/java/pmb/sort/photos/TestUtils.java
+++ src/test/java/pmb/sort/photos/TestUtils.java
@@ -1,12 +1,19 @@
 package pmb.sort.photos;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.jupiter.api.Assertions;
 
 public class TestUtils {
 
     private static final String TEST_RESOURCE_DIR = "src/test/resources/";
     public static final File WITH_EXIF = new File(TEST_RESOURCE_DIR + "test1.jpg");
     public static final File WITHOUT_EXIF = new File(TEST_RESOURCE_DIR + "test2.jpg");
+    public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("i18n", Locale.FRENCH);
 
     public class MySpy {
 
@@ -16,6 +23,10 @@ public class TestUtils {
             System.out.println("hello");
         }
 
+    }
+
+    public static void assertDate(String expected, Date actual) {
+        Assertions.assertEquals(expected, new SimpleDateFormat("yyyy-MM-dd'T'HH:mm").format(actual));
     }
 
 }
