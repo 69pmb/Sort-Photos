c2d619249dab94f8650020ade8b68675d982457d
69pmb <pmbroca@gmail.com>
2020-11-02 17:55:45 +0100

    test: Controller

--- src/test/java/pmb/sort/photos/TestUtils.java
+++ src/test/java/pmb/sort/photos/TestUtils.java
@@ -5,14 +5,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.function.Function;
 
 import org.junit.jupiter.api.Assertions;
 
+import pmb.my.starter.utils.MyConstant;
+
 public class TestUtils {
 
-    private static final String TEST_RESOURCE_DIR = "src/test/resources/";
-    public static final File WITH_EXIF = new File(TEST_RESOURCE_DIR + "test1.jpg");
-    public static final File WITHOUT_EXIF = new File(TEST_RESOURCE_DIR + "test2.jpg");
+    public static final String TEST_RESOURCE_DIR = MyConstant.USER_DIRECTORY + "src/test/resources/";
+    public static final Function<String, File> GET_FILE = name -> new File(TEST_RESOURCE_DIR + name);
+    public static final File WITH_EXIF = GET_FILE.apply("test1.jpg");
+    public static final File WITHOUT_EXIF = GET_FILE.apply("test2.jpg");
     public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("i18n", Locale.FRENCH);
 
     public class MySpy {
