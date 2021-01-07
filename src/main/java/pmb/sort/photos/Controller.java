5b30ceaf6499f3ac283c2fd1fac7a43695be8631
pierre-marie broca <pierre-marie.broca@worldline.com>
2021-01-07 12:01:14 +0100

    refactor: specific duplicate dialog class

--- src/main/java/pmb/sort/photos/Controller.java
+++ src/main/java/pmb/sort/photos/Controller.java
@@ -29,8 +29,6 @@ import org.apache.logging.log4j.Logger;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -39,11 +37,8 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import pmb.my.starter.exception.MinorException;
 import pmb.my.starter.utils.MyConstant;
 import pmb.my.starter.utils.MyFileUtils;
@@ -52,10 +47,10 @@ import pmb.sort.photos.model.Fallback;
 import pmb.sort.photos.model.Picture;
 import pmb.sort.photos.model.Property;
 import pmb.sort.photos.utils.Constant;
-import pmb.sort.photos.utils.JavaFxUtils;
 import pmb.sort.photos.utils.MiscUtils;
 
-public class Controller implements Initializable {
+public class Controller
+        implements Initializable {
 
     private static final Logger LOG = LogManager.getLogger(Controller.class);
 
@@ -362,91 +357,8 @@ public class Controller implements Initializable {
                     && !Files.isSameFile(picture.toPath(), newFile.toPath()))) {
                 Files.move(picture.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             } else if (!Files.isSameFile(picture.toPath(), newFile.toPath())) {
-                duplicateDialog(picture, newPath, new Picture(newFile), count);
+                new DuplicateDialog(container, bundle, picture, newPath, new Picture(newFile), count);
             }
-        }
-    }
-
-    private void duplicateDialog(Picture picture, String newPath, Picture existingPicture, String count) {
-        LOG.debug("Start duplicateDialog");
-        Stage dialog = new Stage();
-        dialog.initOwner(container.getScene().getWindow());
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.setTitle(count);
-        GridPane root = new GridPane();
-        root.setVgap(10);
-        GridPane gridPane = new GridPane();
-        JavaFxUtils.buildScrollPane(gridPane, 500, 700);
-        gridPane.setHgap(10);
-        gridPane.add(JavaFxUtils.displayPicture(picture.getPath(), Constant.CSS_CLASS_BOX, 600), 1, 1);
-        gridPane.add(JavaFxUtils.displayPicture(existingPicture.getPath(), Constant.CSS_CLASS_BOX, 600), 2, 1);
-        gridPane.add(JavaFxUtils.buildText(picture.prettyPrint(bundle), 400), 1, 2);
-        gridPane.add(JavaFxUtils.buildText(existingPicture.prettyPrint(bundle), 400), 2, 2);
-
-        // Buttons
-        HBox hBox = new HBox();
-        hBox.getChildren().add(new Text(bundle.getString(picture.equals(existingPicture) ? "duplicate.warning.equals" : "duplicate.warning")));
-        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.cancel"), e -> {
-            LOG.debug("Do nothing");
-            dialog.close();
-        });
-        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.overwrite"), e -> overwrite(picture, newPath, existingPicture, dialog));
-        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.rename"), e -> renameWithSuffix(picture, newPath, dialog));
-        JavaFxUtils.buildButton(hBox, bundle.getString("duplicate.button.delete"), e -> delete(picture, dialog));
-        hBox.setSpacing(10);
-
-        root.add(gridPane, 1, 1);
-        root.add(hBox, 1, 2);
-        GridPane.setMargin(hBox, new Insets(10));
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(Controller.class.getResource(Constant.CSS_FILE).toExternalForm());
-        dialog.setScene(scene);
-        dialog.showAndWait();
-        LOG.debug("End duplicateDialog");
-    }
-
-    private void delete(Picture picture, Stage dialog) {
-        LOG.debug("Delete");
-        try {
-            dialog.close();
-            Files.deleteIfExists(picture.toPath());
-        } catch (IOException e1) {
-            throw new MinorException("Error deleting file " + picture.getPath(), e1);
-        }
-    }
-
-    private void overwrite(Picture picture, String newPath, Picture existingPicture, Stage dialog) {
-        LOG.debug("Overwrite");
-        try {
-            dialog.close();
-            Files.move(picture.toPath(), existingPicture.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e1) {
-            throw new MinorException("Error moving file " + picture.getPath() + " to " + newPath, e1);
-        }
-    }
-
-    private void renameWithSuffix(Picture picture, String newPath, Stage dialog) {
-        LOG.debug("Suffix");
-
-        Integer index = 1;
-        String bareNewPath = StringUtils.substringBeforeLast(newPath, MyConstant.DOT);
-        String suffix = StringUtils.substringAfterLast(bareNewPath, Constant.SUFFIX_SEPARATOR);
-        if (StringUtils.isNumeric(suffix)) {
-            index = Integer.valueOf(suffix) + 1;
-        }
-        String suffixedPath = StringUtils.substringBeforeLast(bareNewPath, Constant.SUFFIX_SEPARATOR) + Constant.SUFFIX_SEPARATOR + index
-                + MyConstant.DOT + picture.getExtension();
-
-        File suffixedFile = new File(suffixedPath);
-        dialog.close();
-        if (!suffixedFile.exists()) {
-            try {
-                Files.move(picture.toPath(), suffixedFile.toPath());
-            } catch (IOException e1) {
-                throw new MinorException("Error renaming file " + picture.getPath() + " to " + newPath, e1);
-            }
-        } else {
-            duplicateDialog(picture, suffixedPath, new Picture(suffixedFile), "");
         }
     }
 
