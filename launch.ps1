bbcc296c332eed10f408f9d7a8b84e1949b487cd
69pmb <pmbroca@gmail.com>
2021-02-06 10:02:37 +0100

    chore: launcher

--- launch.ps1
+++ launch.ps1
@@ -1,7 +1,14 @@
-cd C:\workspace\Perso\Sort-Photos
-Set-Variable -Name PATH_TO_FX -Value "C:\Program Files\Java\javafx-sdk-11.0.2\lib"
-if (-not (Test-Path -Path target\sort-photos-0.0.1-SNAPSHOT-shaded.jar)) {
-    mvn clean package -"Dmaven.test.skip=true" -U -q
+# Set-Variable -Name PATH_TO_FX -Value "C:\Program Files\Java\javafx-sdk-11.0.2\lib"
+if(!$env:PATH_TO_FX -Or -not (Test-Path -Path $env:PATH_TO_FX)) {
+	Write-Host "Please install Java Fx SDK or define its path in 'PATH_TO_FX' env variable" -ForegroundColor Red
+	pause
+	exit
 }
-java --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml -jar target\sort-photos-0.0.1-SNAPSHOT-shaded.jar
+[xml]$pom = Get-Content pom.xml
+$jar = "target\sort-photos-" + $pom.project.version + "-shaded.jar"
+if (-not (Test-Path -Path $jar)) {
+    Write-Host "Building jar.." -ForegroundColor Blue
+	mvn clean package -"Dmaven.test.skip=true" -U -q
+}
+java --module-path $env:PATH_TO_FX --add-modules javafx.controls,javafx.fxml -jar $jar
 pause
